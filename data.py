3001bb3cc84e2a1ba0cefd79f64a003141f46f6b
Prachii-07 <ak4765486@gmail.com>
2026-07-08 11:14:59 +0530

    Trained Logistic regression and added decision tree

--- data.py
+++ data.py
@@ -23,3 +23,42 @@ df['Happy'] = (df['Happiness Score'] > median_score).astype(int)
 
 print(df['Happy'].value_counts())
 
+#Encode Region(text) into numbers
+le = LabelEncoder()
+df['Region_Encoded'] = le.fit_transform(df['Region'])
+
+#Select features(X) and target (y)
+features= ['Economy', 'Family', 'Health', 'Freedom', 'Corruption', 'Generosity','Job Satisfaction', 'Region_Encoded']
+X = df[features]
+y = df['Happy']
+
+#Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#Scale the features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+#Train the model
+model = LogisticRegression()
+model.fit(X_train_scaled, y_train)
+
+#Predict and check accuracy
+y_pred = model.predict(X_test_scaled)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy*100, "%")
+
+from sklearn.tree import DecisionTreeClassifier
+
+# Train a Decision Tree Model
+dt_model = DecisionTreeClassifier(max_depth=3,random_state=42)
+dt_model.fit(X_train_scaled, y_train)
+
+# Predict and check accuracy
+y_pred_dt = dt_model.predict(X_test_scaled)
+accuracy_dt = accuracy_score(y_test, y_pred_dt)
+
+print("\n----Model Comparison----")
+print("Logistic Regression Accuracy:", accuracy*100, "%")
+print("Decision Tree Accuracy:", accuracy_dt*100, "%")
